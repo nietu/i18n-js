a9003c0b66c4b04bbb6a01d1b8797c178a863023
Tommi Niemi <nietu@pcuf.fi>
2015-05-25 19:32:59 +0300

    due this https://github.com/svenfuchs/i18n/issues/254

--- lib/i18n/js.rb
+++ lib/i18n/js.rb
@@ -149,13 +149,37 @@ module I18n
     end
 
     # Initialize and return translations
+    # forked due https://github.com/fnando/i18n-js/issues/59
     def self.translations
-      ::I18n.backend.instance_eval do
-        init_translations unless initialized?
-        translations.slice(*::I18n.available_locales)
-      end
-    end
+      all_translations = {}
+      selected_backends = []
+      current_backend = ::I18n.backend
 
+      case current_backend
+      when I18n::Backend::Chain
+        current_backend.backends.each do |backend_in_chain|
+          if backend_in_chain.respond_to?(:translations, true)
+            selected_backends << backend_in_chain
+          end
+        end
+      else
+        if current_backend.respond_to?(:translations, true)
+          selected_backends = [current_backend] 
+        end
+      end
+
+      selected_backends.each do |selected_backend|
+        selected_backend.instance_eval do
+          # all `selected_backends` are `::I18n::Backend::Simple`
+          if defined?(:initialized?) && defined?(:init_translations)
+            init_translations unless initialized?
+          end
+        end
+        all_translations.deep_merge!(selected_backend.send(:translations))
+      end 
+
+      all_translations
+    end
     def self.use_fallbacks?
       fallbacks != false
     end
